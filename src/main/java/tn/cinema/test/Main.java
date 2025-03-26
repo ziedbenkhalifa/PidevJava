d881211c2a20d2ad612e1c919a1a884f21352a90
abdelraouf18 <abdelraouf.ayachi@esprit.tn>
2025-03-26 21:56:44 +0100

    gestion films

--- src/main/java/tn/cinema/test/Main.java
+++ src/main/java/tn/cinema/test/Main.java
@@ -3,7 +3,7 @@ package tn.cinema.test;
 import tn.cinema.tools.Mydatabase;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Mydatabase connexion = Mydatabase.getInstance();
 
 
