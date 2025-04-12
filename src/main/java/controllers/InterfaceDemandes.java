2531df7eb042a17b5a631e38c95f74fbee480f21
ziedbenkhalifa <zied8758@gmail.com>
2025-04-12 14:14:42 +0100

    crud publicite interface

--- src/main/java/controllers/InterfaceDemandes.java
+++ src/main/java/controllers/InterfaceDemandes.java
@@ -1,4 +1,44 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import tn.cinema.entities.Demande;
+
+import java.sql.Date;
+
 public class InterfaceDemandes {
+    @FXML
+    private TableColumn<?, ?> Actions;
+
+    @FXML
+    private TableColumn<Demande, Integer> AdminId;
+
+    @FXML
+    private TableColumn<Demande, Date> date;
+
+    @FXML
+    private TableColumn<Demande, String> description;
+
+    @FXML
+    private TableColumn<Demande, Integer> id;
+
+    @FXML
+    private TableColumn<Demande, String> liensupp;
+
+    @FXML
+    private TableColumn<Demande, Integer> nbrjours;
+
+    @FXML
+    private TableColumn<Demande, String> statut;
+
+    @FXML
+    private TableView<Demande> tableauDemande;
+
+    @FXML
+    private TableColumn<Demande, String> type;
+
+    @FXML
+    private TableColumn<Demande, Integer> userId;
+
 }
