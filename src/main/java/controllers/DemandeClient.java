7fa0857b0cc51059422b1dd0559e828b9f733b9c
ziedbenkhalifa <zied8758@gmail.com>
2025-04-13 22:04:51 +0100

    modifications interface front

--- src/main/java/controllers/DemandeClient.java
+++ src/main/java/controllers/DemandeClient.java
@@ -1,4 +1,43 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class DemandeClient {
+    @FXML
+    private Button publicitesButton;
+    @FXML
+    private Button demandeSubButton;
+    @FXML
+    private Button publiciteSubButton;
+
+    @FXML
+    public void toggleSubButtons(ActionEvent event) {
+        boolean isVisible = demandeSubButton.isVisible();
+        demandeSubButton.setVisible(!isVisible);
+        publiciteSubButton.setVisible(!isVisible);
+    }
+
+    @FXML
+    public void goToDemandeClient(ActionEvent event) throws IOException {
+        // Already on DemandeClient, no action needed
+    }
+
+    @FXML
+    public void goToPubliciteClient(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/PubliciteClient.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- src/main/java/controllers/PubliciteClient.java
+++ src/main/java/controllers/PubliciteClient.java
@@ -1,4 +1,44 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class PubliciteClient {
+    @FXML
+    private Button publicitesButton;
+    @FXML
+    private Button demandeSubButton;
+    @FXML
+    private Button publiciteSubButton;
+
+    @FXML
+    public void toggleSubButtons(ActionEvent event) {
+        boolean isVisible = demandeSubButton.isVisible();
+        demandeSubButton.setVisible(!isVisible);
+        publiciteSubButton.setVisible(!isVisible);
+    }
+
+    @FXML
+    public void goToDemandeClient(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/DemandeClient.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void goToPubliciteClient(ActionEvent event) throws IOException {
+        // Already on PubliciteClient, no action needed
+    }
+
+
 }
